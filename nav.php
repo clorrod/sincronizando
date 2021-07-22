44a81310e89c4dd5d3a21ed3b886c38e37aa4153
clorrod <cesar.tci@gmail.com>
2021-07-22 21:45:45 +0200

    modificando localmente el archivo NAV

---
+++ nav.php
@@ -0,0 +1,17 @@
+<body>
+    <h1>Listas de enlaces</h1>
+    <ul>
+      <li>Buscadores
+        <ul>
+          <li><a href="http://www.bing.com/" target="_blank">Bing</a></li>
+          <li><a href="http://www.google.com/" target="_blank">Google</a></li>
+        </ul>
+      </li>
+      <li>Redes sociales
+        <ul>
+          <li><a href="http://www.facebook.com/" target="_blank">Facebook</a></li>
+          <li><a href="http://www.twitter.com/" target="_blank">Twitter</a></li>
+        </ul>
+      </li>
+    </ul>
+  </body>
